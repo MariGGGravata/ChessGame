9a35c91631ea54e1083173a64e2f69212f2fb91e
MariGravata <marig.gravata@gmail.com>
2025-04-30 14:36:59 -0300

    Init_01 : First adjustments and creation of classes related to the game.

---
+++ Board/Part.cs
@@ -0,0 +1,25 @@
+﻿using Board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.Board
+{
+    public class Part
+    {
+        public Position Position { get; set; }
+        public Colour Colour { get; set; }
+        public int QtyMove { get; set; }
+        public Board Board { get; set; }
+
+        public Part(Position position, Board board, Colour colour)
+        {
+            this.Position = position;
+            this.Board = board;
+            this.Colour = colour;
+            this.QtyMove = 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Board;
+
+namespace ChessGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Chess Game!");
+            // Initialize the game and start playing
+
+            Position position;
+        }
+    }
+}
